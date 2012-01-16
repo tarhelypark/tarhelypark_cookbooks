3365d48dd045d955c4c48bb424168d7a6780b167
Gabor Almer <almergabor@gmail.com>
2012-01-16 14:49:52 +0100

    File encoding UTF8 and Readme in markdown

--- atop/metadata.rb
+++ atop/metadata.rb
@@ -1,8 +1,9 @@
+# encoding: UTF-8
 maintainer       "Tárhelypark"
 maintainer_email "office@tarhelypark.hu"
 license          "Apache 2.0"
 description      "Installs atop"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "build-essential"
